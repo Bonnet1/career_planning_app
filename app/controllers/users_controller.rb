35804655e6820a21e2277a577882bd80aa23391c
Bonnet1 <c.mannion84@gmail.com>
2019-10-13 13:13:20 -0400

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Fifty Backwards Career Planning App!"
       redirect_to @user
     else
